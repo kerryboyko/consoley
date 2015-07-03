73d2a2a381ad26428d6d16efbbe3becd0c7b5c50
brianboyko <brian.boyko@gmail.com>
2015-07-03 12:13:55 -0500

    Got it to work for apt get!

--- js/consoley.js
+++ js/consoley.js
@@ -1,82 +1,46 @@
 var aptgetString = "sudo apt-get install ";
-var installNames = [];
 var listOfPackages = [];
 
 var jsonLoad = function() {
-    listOfPackages = $.getJSON(
-        "http://brianboyko.github.io/consoley/packages.json", function(
-            result) {});
+
+
+    $.getJSON("http://brianboyko.github.io/consoley/packages.json", function(data) {
+        var items = [];
+        $.each(data, function(key, val) {
+            if (val.repo == "heading") {
+                items.push("<h3>" + val.engName + "</h3>");
+            } else {
+                items.push('<img src="images/' + val.pakName +
+                    '.png" />' + '<input class="' + val.repo +
+                    '" type="checkbox" id="' + val.pakName +
+                    '">' + val.engName + '</input>' + '<p>' +
+                    val.description + '</p><br />');
+            }
+        });
+        $("<ul/>", {
+            "class": "my-new-list",
+            html: items.join("")
+        }).appendTo(document.getElementById('checkboxes'));
+    });
+
 };
-
-$.getJSON("http://brianboyko.github.io/consoley/packages.json", function(data) {
-    var items = [];
-    $.each(data, function(key, val) {
-        if (val.repo == "heading") {
-            items.push("<h3>" + val.engName + "</h3>");
-        } else {
-            items.push('<img src="images/' + val.pakName +
-                '.png" />' + '<input class="' + val.repo +
-                '" type="checkbox" id="' + val.pakName +
-                '">' + val.engName + '</input>' + '<p>' +
-                val.description + '</p><br />');
-        }
-    });
-    $("<ul/>", {
-        "class": "my-new-list",
-        html: items.join("")
-    }).appendTo(document.getElementById('checkboxes'));
-});
-
-var allPackages = function(list) { // takes an array of objects from JSON
-    var checkboxGenerator = "";
-    console.log("is it getting here");
-    list.forEach(function() {
-        if (list[i].repo == "heading") {
-            var newH3 = document.createElement('h3');
-            newH3.innerHTML = list[i].engName;
-            document.getElementbyId('checkboxes').appendChild(
-                newH3);
-        } else {
-            var newDiv = document.createElement('div');
-            newDiv.innerHTML = '<img src="images/' + list[i].pakName +
-                '.png" />"' + '<input class="' + list[i].repo +
-                '" type="checkbox" id="' + list[i].pakName + '">' +
-                list[i].engName + '</input>' + '<p>' + list[i].description +
-                '</p><br />';
-            document.getElementbyId('checkboxes').appendChild(
-                newDiv);
-        } //endif
-    });
-    //endfor
-    return checkboxGenerator; // returns a string.
-}; //end allPackages()
 
 window.onload = function() {
     jsonLoad();
     var boxes = document.forms.checkboxes;
-    boxes.addEventListener("click", generateApt, true);
     var elements = document.getElementsByClassName('aptget');
-    for (var i = 0; i < elements.length; i++) {
-        installNames[i] = elements[i].getAttribute('id');
-    }
+    boxes.addEventListener("click", generateApt, true);
 };
-
-function addAptString(element, index, array) {
-    if (document.getElementById(element).checked) {
-        aptgetString = aptgetString + element + " ";
-    }
-}
 
 function generateApt(ev) {
     aptgetString = "sudo apt-get install ";
 
-    installNames.forEach(addAptString);
-
     var allCheckboxes = document.getElementsByClassName('aptget');
-    noneChecked = true;
+    var noneChecked = true;
     for (var i = 0; i < allCheckboxes.length; i++) {
         if (allCheckboxes[i].checked) {
             noneChecked = false;
+            aptgetString = aptgetString + allCheckboxes[i].id + " ";
         } //endif
     } //endfor
     if (!noneChecked) {
