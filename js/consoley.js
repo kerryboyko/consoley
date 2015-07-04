d4f0e05acb5fc2bd7538cdde42369f5fb4d512dd
BrianBoyko <brian.boyko@gmail.com>
2015-07-04 11:56:14 -0500

    finalwork decided to do apt-get only

--- js/consoley.js
+++ js/consoley.js
@@ -4,24 +4,29 @@ var listOfPackages = [];
 var jsonLoad = function() {
 
 
-    $.getJSON("http://brianboyko.github.io/consoley/packages.json", function(data) {
-        var items = [];
-        $.each(data, function(key, val) {
-            if (val.repo == "heading") {
-                items.push("<h3>" + val.engName + "</h3>");
-            } else {
-                items.push('<img src="images/' + val.pakName +
-                    '.png" />' + '<input class="' + val.repo + ' ' + val.distros +
-                    '" type="checkbox" id="' + val.pakName +
-                    '">' + '<span class="' + val.repo + ' ' + val.distros + '">' + val.engName + '</span></input>' + '<p class="'+ val.repo + ' ' + val.distros + '">' +
-                    val.description + '</p><br />');
-            }
+    $.getJSON("http://brianboyko.github.io/consoley/packages.json",
+        function(data) {
+            var items = [];
+            $.each(data, function(key, val) {
+                if (val.repo == "heading") {
+                    items.push("<h3>" + val.engName + "</h3>");
+                } else {
+                    items.push('<img src="images/' + val.pakName +
+                        '.png" />' + '<input class="' + val
+                        .repo + ' ' + val.distros +
+                        '" type="checkbox" id="' + val.pakName +
+                        '">' + '<span class="' + val.repo +
+                        ' ' + val.distros + '">' + val.engName +
+                        '</span></input>' + '<p class="' +
+                        val.repo + ' ' + val.distros + '">' +
+                        val.description + '</p><br />');
+                }
+            });
+            $("<ul/>", {
+                "class": "my-new-list",
+                html: items.join("")
+            }).appendTo(document.getElementById('checkboxes'));
         });
-        $("<ul/>", {
-            "class": "my-new-list",
-            html: items.join("")
-        }).appendTo(document.getElementById('checkboxes'));
-    });
 
 };
 
